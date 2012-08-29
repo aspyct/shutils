d6f4fcd2877790a14c52d63ff48fadf825a3d78d
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2012-08-29 22:53:08 +0200

    Added warning if a requirement is not met

--- shutils_install_osx.sh
+++ shutils_install_osx.sh
@@ -4,9 +4,9 @@
 cd `dirname $0`
 dir=`pwd`
 profile=~/.bash_profile
+requirements="php"
 
 line="export PATH=\$PATH:$dir"
-
 if [ -f $profile ]; then
     found=`grep "$line" $profile`
 else
@@ -18,7 +18,20 @@ if [ "$found" -a "$1" != "-f" ]; then
     echo "ERROR: Try sourcing the profile: \". $profile\""
     echo "ERROR: Re-run with -f to force if it still doesn't work"
 else
+    for req in $requirements
+    do
+        prog=`which $req`
+
+        if [ "$prog" ]; then
+            echo "Using $prog"
+        else
+            echo "WARNING: Some of these scritps require '$req'"
+        fi
+    done
+
     echo $line >> $profile
     echo "Added \"$line\" to your `basename $profile`. Please source it now"
     echo "-> run \". $profile\""
 fi
+
+
