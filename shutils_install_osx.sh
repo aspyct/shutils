1687f6602d297f59a79edd6d8321e8f6b22f0224
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2012-08-29 22:48:36 +0200

    Modified the install to create bash_profile instead of profile

--- shutils_install_osx.sh
+++ shutils_install_osx.sh
@@ -3,17 +3,22 @@
 # Add this directory to the $PATH if not yet in the profile
 cd `dirname $0`
 dir=`pwd`
-profile=~/.profile
+profile=~/.bash_profile
 
 line="export PATH=\$PATH:$dir"
-found=`grep "$line" $profile`
+
+if [ -f $profile ]; then
+    found=`grep "$line" $profile`
+else
+    found=""
+fi
 
 if [ "$found" -a "$1" != "-f" ]; then
-    echo "ERROR: Looks like the path is already in your .profile"
+    echo "ERROR: Looks like the path is already in your `basename $profile`"
     echo "ERROR: Try sourcing the profile: \". $profile\""
     echo "ERROR: Re-run with -f to force if it still doesn't work"
 else
     echo $line >> $profile
-    echo "Added \"$line\" to your $profile. Please source it now"
+    echo "Added \"$line\" to your `basename $profile`. Please source it now"
     echo "-> run \". $profile\""
 fi
