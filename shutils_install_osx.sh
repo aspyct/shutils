a36b1ac3854e2f88062362c678ced0293fc40110
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2012-08-29 22:41:49 +0200

    Revised install script

--- shutils_install_osx.sh
+++ shutils_install_osx.sh
@@ -1,21 +1,19 @@
 #!/bin/sh
 
-# Add this directory to the $PATH if not yet in
+# Add this directory to the $PATH if not yet in the profile
 cd `dirname $0`
 dir=`pwd`
+profile=~/.profile
 
 line="export PATH=\$PATH:$dir"
-found=`grep "$line" ~/.profile`
+found=`grep "$line" $profile`
 
-if [ "$found" ]; then
-    if [ "$1" != "-f" ]; then
-        echo "Looks like the path is already in your .profile"
-        echo "\tTry sourcing the profile: \". ~/.profile\""
-        echo "\tRe-run with -f to force if it still doesn't work"
-        exit
-    fi
+if [ "$found" -a "$1" != "-f" ]; then
+    echo "ERROR: Looks like the path is already in your .profile"
+    echo "ERROR: Try sourcing the profile: \". $profile\""
+    echo "ERROR: Re-run with -f to force if it still doesn't work"
+else
+    echo $line >> $profile
+    echo "Added \"$line\" to your $profile. Please source it now"
+    echo "-> run \". $profile\""
 fi
-
-echo $line >> ~/.profile
-echo "Added \"$line\" to your ~/.profile. Please source it now"
-echo "-> run \". ~/.profile\""
